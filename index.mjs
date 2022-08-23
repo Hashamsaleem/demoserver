bb7e40f4b56e06df3d61aa069fd878bb8ad00fc0
Hashm01 <hashamsaleem75@gmail.com>
2022-08-23 11:54:45 +0500

    completed

--- index.mjs
+++ index.mjs
@@ -1,9 +1,28 @@
 import express from "express";
+import cors from "cors";
 const app = express();
 const port = 3000;
 
+app.use(cors());
+
 app.get("/", (req, res) => {
+  res.send("Home page");
+});
+
+app.get("/home", (req, res) => {
   res.send("Hasham Saleem");
+});
+
+app.get("/weather", (req, res) => {
+  res.send({
+    name: "karachi",
+    time: 12,
+    env: "rainy",
+  });
+});
+
+app.get("/profile", (req, res) => {
+  res.send("abdul Saleem");
 });
 
 app.listen(port, () => {
